0b9fba2415159e95972d81039002974f58a1a78c
sri-vat-san <sriarj88@gmail.com>
2016-01-10 23:54:33 -0800

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,23 @@
+source("load_data.R")
+
+plot3 <- function(data = NULL) {
+    if(is.null(data)) {
+        data <- load_data()
+    }
+    
+    png("plot3.png", width = 480, height = 480)
+    
+    plot(data$Time, data$Sub_metering_1, 
+         type = "l", 
+         xlab = "", 
+         ylab = "Energy sub metering",
+         col = "black")
+    lines(data$Time, data$Sub_metering_2, col = "red")
+    lines(data$Time, data$Sub_metering_3, col = "blue")
+    legend("topright",
+           col = c("black", "red", "blue"),
+           legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+           lty = 1)
+    
+    dev.off()
+}
